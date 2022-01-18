e18355710ff2726f79fc98bf9f1fcb2c5381dfa1
sangeun.seong <sangeun.seong@timf.co.kr>
2022-01-18 16:16:10 +0900

    first commit

---
+++ src/test/java/com/login/login_form/LoginFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.login_form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
